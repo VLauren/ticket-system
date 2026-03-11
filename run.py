0df8a297e36b3d5d33e74aaff49781c3ab75c021
VLauren <laurentino.vilchez@gmail.com>
2026-03-11 13:20:29 +0100

    add route to display reservation form

--- run.py
+++ run.py
@@ -1,10 +1,8 @@
 from flask import Flask
+from app.routes import main
 
 app = Flask(__name__)
-
-@app.route('/')
-def test():
-    return 'Test'
+app.register_blueprint(main)
 
 if __name__ == '__main__':
     app.run(debug=True)
