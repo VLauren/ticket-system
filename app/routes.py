16e1181ec501151a2fc114f5ec8a35dbb851d8da
VLauren <laurentino.vilchez@gmail.com>
2026-03-12 09:16:21 +0100

    reservation endpoint

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,17 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from .utils import generate_ticket_id
 
 main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+@main.route('/reserve', methods=['POST'])
+def reserve():
+    name = request.form['name']
+    email = request.form['email']
+    
+    ticket_id = generate_ticket_id()
+
+    return f"Recibido reserva para {name} con ticket {ticket_id}"
