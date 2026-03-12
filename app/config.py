72df7fa4922cbc63a596090f7f9d71a430d6bc59
VLauren <laurentino.vilchez@gmail.com>
2026-03-12 10:11:42 +0100

    flask mail

---
+++ app/config.py
@@ -0,0 +1,8 @@
+MAIL_SERVER = "smtp.gmail.com"
+MAIL_PORT = 587
+MAIL_USE_TLS = True
+
+MAIL_USERNAME = "mail@gmail.com"
+MAIL_PASSWORD = "app_password"
+
+MAIL_DEFAULT_SENDER = "mail@gmail.com"
--- run.py
+++ run.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_mail import Mail
 from app.routes import main
 
 app = Flask(__name__)
+app.config.from_object("app.config")
+
+mail = Mail(app)
+
 app.register_blueprint(main)
 
 if __name__ == '__main__':
