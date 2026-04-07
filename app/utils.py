5a66aa6589a30afc533583f6dd37391b1e8d2271
VLauren <laurentino.vilchez@gmail.com>
2026-04-07 10:59:42 +0200

    ticket format + show date in day selection dropdown

--- app/utils.py
+++ app/utils.py
@@ -39,20 +39,40 @@ def generate_ticket_pdf(ticket_id, name, day):
     # pdf_path = f"tickets/{ticket_id}.pdf"
     pdf_buffer = io.BytesIO()
 
+    dates = {1: "29 de Mayo", 2: "30 de Mayo"}
+    date_str = dates.get(int(day), f"Día {day}")
+
     c = canvas.Canvas(pdf_buffer, pagesize=A6)
 
     c.setFont("Helvetica-Bold", 16)
-    c.drawCentredString(297/2, 420-50, "ENTRADA")
+    c.drawCentredString(297/2, 420-60, "ENTRADA")
 
+    c.setFont("Helvetica-Bold", 14)
+    c.drawCentredString(297/2, 420-80, f"Día {day} - {date_str}")
+
+    c.setFont("Helvetica", 11)
+    c.drawCentredString(297/2, 420-120, f"Caja Blanca 19:00 (apertura de puertas 18:30)")
+
+    c.line(35, 420-145, 262, 420-145)
+
+    c.setFont("Helvetica-Bold", 12)
+    c.drawString(35, 420-175, "Nombre:")
     c.setFont("Helvetica", 12)
-    c.drawString(30, 330, f"Día {day}")
-    c.drawString(30, 310, f"Nombre: {name}")
-    c.drawString(30, 280, "Codigo de entrada:")
-    c.setFont("Helvetica", 10)
-    c.drawString(30, 260, f"{ticket_id}")
+    c.drawString(95, 420-175, name)
+
+    # c.setFont("Helvetica", 12)
+    # c.drawString(30, 330, f"Día {day}")
+    # c.drawString(30, 310, f"Nombre: {name}")
+    # c.drawString(30, 280, "Codigo de entrada:")
+    # c.setFont("Helvetica", 10)
+    # c.drawString(30, 260, f"{ticket_id}")
 
     qr_image = ImageReader(qr_buffer)
     c.drawImage(qr_image, (297/2)-(150/2), 60, width=150, height=150)
+    # c.drawImage(qr_image, (297/2)-(120/2), 100, width=120, height=120)
+
+    c.setFont("Helvetica", 9)
+    c.drawCentredString(297/2, 50, f"ID: {ticket_id}")
 
     c.showPage()
     c.save()
